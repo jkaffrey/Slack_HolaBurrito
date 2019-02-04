b975b3d1e87e299a80a7de68376093c590d5aee5
Jeremy Kaffrey <jkaffrey@gmail.com>
2019-02-04 15:45:27 -0700

    Cleaning up send message

--- index.js
+++ index.js
@@ -11,17 +11,27 @@ let slack = ts.instance({ token: TOKEN });
 
 // event handler
 slack.on('reaction_added', 'message.groups', 'message.channels', payload => {
-  let {type, user, item} = payload.event;
-  let message = 'Hello';
+  //let {type, user, item} = payload.event;
+  //let message = 'Hello';
   
   console.log("Testing against");
-  console.log(payload);
+  console.log(payload.event);
+  
+  if (payload.event.reaction === 'burrito') {
+	  var message = {
+		  unfurl_links: true,
+		  channel: payload.event.user,
+		  token: TOKEN,
+		  text: 'You gave <@' + payload.event.user + '> a burrito'
+	  }
+	  slack.send(message);
+  }
   
   // save the message and update the timestamp
-  slack.send(message).then(res => {
-    let {ts, channel} = res.data;
-    users[user] = Object.assign({}, message, { ts: ts, channel: channel });
-  });
+  // slack.send(message).then(res => {
+    //let {ts, channel} = res.data;
+    //users[user] = Object.assign({}, message, { ts: ts, channel: channel });
+  // });
 });
 
 
