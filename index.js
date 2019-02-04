65d8ef47d87e362d233cc2675502e88f65a8adb4
Jeremy Kaffrey <jkaffrey@gmail.com>
2019-02-04 14:36:21 -0700

    retracking files

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const ts = require('tinyspeck'),
+      onboarding = require('./onboarding.json'),
       {PORT, TOKEN} = process.env,
       users = {};
 
 
 // setting defaults for all Slack API calls
 let slack = ts.instance({ token: TOKEN });
+
+
+// build the user's current onboarding message
+function getStatusMessage(user) {
+  return Object.assign({ channel: user }, onboarding.welcome, users[user]);
+}
+
+
+// watch for onboarding slash commands
+slack.on('/onboarding', payload => {
+  let {user_id, response_url} = payload;
+  let message = getStatusMessage(user_id);
+
+  // send current onboarding status privately
+  slack.send(response_url, message);
+});
+
 
 // event handler
 slack.on('star_added', 'pin_added', 'reaction_added', payload => {
