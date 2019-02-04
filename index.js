8863427561fe732184ce5d39fd363eb2ed935142
Jeremy Kaffrey <jkaffrey@gmail.com>
2019-02-04 15:05:35 -0700

    Modified

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
+require('dotenv').config();
 const ts = require('tinyspeck'),
-      onboarding = require('./onboarding.json'),
-      {PORT, TOKEN} = process.env,
+      PORT = process.env.PORT || 8080,
+	  TOKEN = process.env.TOKEN,
       users = {};
 
+	  console.log(TOKEN);
 
 // setting defaults for all Slack API calls
 let slack = ts.instance({ token: TOKEN });
 
-
-// build the user's current onboarding message
-function getStatusMessage(user) {
-  return Object.assign({ channel: user }, onboarding.welcome, users[user]);
-}
-
-
-// watch for onboarding slash commands
-slack.on('/onboarding', payload => {
-  let {user_id, response_url} = payload;
-  let message = getStatusMessage(user_id);
-
-  // send current onboarding status privately
-  slack.send(response_url, message);
-});
-
-
 // event handler
-slack.on('star_added', 'pin_added', 'reaction_added', payload => {
+slack.on('star_added', payload => {
   let {type, user, item} = payload.event;
   let message = 'Hello';
+  
+  console.log(payload);
   
   // save the message and update the timestamp
   slack.send(message).then(res => {
@@ -38,4 +25,4 @@ slack.on('star_added', 'pin_added', 'reaction_added', payload => {
 
 
 // incoming http requests
-slack.listen();
+slack.listen(PORT);
