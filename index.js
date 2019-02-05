209049656b27eb6e23603b6c7f0bc9cdfef70b54
Jeremy Kaffrey <jkaffrey@gmail.com>
2019-02-05 12:24:30 -0700

    More things

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
 require('dotenv').config();
 const ts = require('tinyspeck'),
+	  fs = require('fs');
       PORT = process.env.PORT || 8080,
 	  BOT_TOKEN = process.env.BOT_TOKEN,
 	  TOKEN = process.env.TOKEN,
 	  REQUEST_URL =  process.env.REQUEST_URL,
+	  MAX_BURRITOS_PER_DAY = process.env.MAX_BURRITOS_PER_DAY,
       users = {};
-
+	  
 // setting defaults for all Slack API calls
 let slack = ts.instance({ token: BOT_TOKEN });
 
-// event handler
-//'reaction_added', 'message', 'message.channels'
-slack.on('reaction_added', 'message', payload => {
-  //let {type, user, item} = payload.event;
-  //let message = 'Hello';
+// fs.writeFile('burritosPerDay.txt', '');
+// fs.writeFile('burritosRecieved.txt', '');
+
+function burritoGiven(fromUser, toUser) {
+	
+	console.log('Start');
+	var recievedFileName = toUser + '_recieved.txt';
+	var hasFile = fs.existsSync(recievedFileName);
+	if (!hasFile) {
+		fs.writeSync(recievedFileName, '0');
+	}
+	
+	var content = fs.readSync(recievedFileName);
+	console.log(content);
+};
+
+slack.on('message', payload => {
   
-  console.log("Testing against");
-  console.log(payload.event.type);
-  console.log(payload);
-  
-  if (payload.event.reaction === 'burrito') {
-	  
-	  console.log('inside');
-	  
-	  var message = {
-		  channel: payload.event.item.channel,
-		  token: TOKEN,
-		  text: 'You gave <@' + payload.event.user + '> a burrito'
-	  }
-	  
-	  slack.send(REQUEST_URL, { text: 'Hello you gave a burrito to ' }).then(res => {
-		  console.log( 'Successfully answered the command' );
-	  }).catch(console.error);
-  } else if (payload.event.text && payload.event.text.indexOf(':burrito:') > 0) {
+  if (payload.event.text && payload.event.text.indexOf(':burrito:') > 0) {
 	  
 	  var userGivenBurrito = payload.event.text.match(/<(.*)>/);
 	  console.log(userGivenBurrito[1]);
 	  console.log(payload.event.user);
 	  
-	  slack.send({ token: BOT_TOKEN, text: 'Hola, you gave a burrito to ' + userGivenBurrito[0], channel: payload.event.user, as_user: false, username: 'Hola Burrito' }).then(res => {
+	  slack.send('chat.postEphemeral', { token: BOT_TOKEN, text: 'Hola, you gave a burrito to ' + userGivenBurrito[0], channel: payload.event.user, as_user: false, username: 'Hola Burrito' }).then(res => {
 		  console.log( 'Successfully answered the command' );
 	  }).catch(console.error);
 	  
 	  slack.send({ token: BOT_TOKEN, text: 'Hola, you recieved a burrito from <@' + payload.event.user + '>', channel: userGivenBurrito[1], as_user: false, username: 'Hola Burrito' }).then(res => {
+		  burritoGiven(payload.event.user, userGivenBurrito[1].replace('@', ''));
 		  console.log( 'Successfully answered the command' );
 	  }).catch(console.error);
   }
