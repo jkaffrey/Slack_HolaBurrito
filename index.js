2f3e36f3f6ab617849e4202ac8999e346fca4515
Jeremy Kaffrey <jkaffrey@gmail.com>
2019-02-05 14:59:54 -0700

    Burrito things

--- index.js
+++ index.js
@@ -14,27 +14,29 @@ let slack = ts.instance({ token: BOT_TOKEN });
 // fs.writeFile('burritosPerDay.txt', '');
 // fs.writeFile('burritosRecieved.txt', '');
 
-function burritoGiven(fromUser, toUser) {
+function burritoGiven(fromUser, toUser, numberGiven) {
 	
-	console.log(fromUser + "  " + toUser);
-	var recievedFileName = toUser + '_recieved.txt';
-	var givenFileName = fromUser + '_given.txt';
-	
-	var hasFileRecieved = fs.existsSync(recievedFileName);
-	if (!hasFileRecieved) {
-		fs.writeFileSync(recievedFileName, '');
+	for (var i = 0; i < numberGiven; i++) {
+		
+		var recievedFileName = toUser + '_recieved.txt';
+		var givenFileName = fromUser + '_given.txt';
+		
+		var hasFileRecieved = fs.existsSync(recievedFileName);
+		if (!hasFileRecieved) {
+			fs.writeFileSync(recievedFileName, '');
+		}
+		
+		var hasFileGiven = fs.existsSync(givenFileName);
+		if (!hasFileGiven) {
+			fs.writeFileSync(givenFileName, '');
+		}
+		
+		var contentRecieved = fs.readFileSync(recievedFileName, 'utf8');
+		fs.writeFileSync(recievedFileName, contentRecieved + ',' + fromUser);
+		
+		var contentGiven = fs.readFileSync(givenFileName, 'utf8');
+		fs.writeFileSync(givenFileName, contentGiven + ',' + toUser);
 	}
-	
-	var hasFileGiven = fs.existsSync(givenFileName);
-	if (!hasFileGiven) {
-		fs.writeFileSync(givenFileName, '');
-	}
-	
-	var contentRecieved = fs.readFileSync(recievedFileName, 'utf8');
-	fs.writeFileSync(recievedFileName, contentRecieved + ',' + fromUser);
-	
-	var contentGiven = fs.readFileSync(givenFileName, 'utf8');
-	fs.writeFileSync(givenFileName, contentGiven + ',' + toUser);
 };
 
 function burritosRemainingPerDay(user) {
@@ -87,8 +89,7 @@ function getAllUsersInStr(str) {
 	var arr = str.split(" ");
 	for (var i = 0; i < arr.length; i++) {
 		
-		var userGivenBurrito = arr[i].match(/<(.*)>/);
-		console.log(userGivenBurrito);
+		var userGivenBurrito = arr[i].match(/<@(.*)>/);
 		if (userGivenBurrito && userGivenBurrito[1]) {
 			
 			outputUsers.push(userGivenBurrito[1]);
@@ -102,34 +103,49 @@ slack.on('message', payload => {
   
   if (payload.event.text && payload.event.text.indexOf(':burrito:') > 0) {
 	  
-	  var userGivenBurrito = payload.event.text.match(/<(.*)>/);
+	  // var userGivenBurrito = payload.event.text.match(/<(.*)>/);
 	  // console.log(userGivenBurrito[1]);
 	  // console.log(payload.event.user);
 	  console.log(getAllUsersInStr(payload.event.text));
+	  var usersGivenBurritos = getAllUsersInStr(payload.event.text);
+	  var burritosGiven = burritosInMention(payload.event.text);
+	  var burritosToDistribute = (usersGivenBurritos.length - 1) * (burritosGiven.length - 1);
 	  
-	  if (payload.event.user === userGivenBurrito[1].replace('@', '')) {
-		  slack.send({ token: BOT_TOKEN, text: 'You cannot give yourself a burrito.', channel: payload.event.user, as_user: false, username: 'Hola Burrito' }).then(res => {
+	  if (burritosToDistribute > burritosRemainingPerDay(payload.event.user)) {
+		  
+		  slack.send({ token: BOT_TOKEN, text: 'You don\'t have enough burritos to give to everyone.', channel: payload.event.user, as_user: false, username: 'Hola Burrito' }).then(res => {
 		  //console.log( 'Successfully answered the command' );
 		  }).catch(console.error);
 		  return;
 	  }
 	  
-	  if (burritosRemainingPerDay(payload.event.user) <= 0) {
-		  slack.send({ token: BOT_TOKEN, text: 'You are out of burritos to give today.', channel: payload.event.user, as_user: false, username: 'Hola Burrito' }).then(res => {
-		  //console.log( 'Successfully answered the command' );
+	  for (var i = 0; i < usersGivenBurritos.length; i++) {
+	  
+		  var userGivenBurrito = usersGivenBurritos[i];
+		  if (payload.event.user === userGivenBurrito) {
+			  slack.send({ token: BOT_TOKEN, text: 'You cannot give yourself a burrito.', channel: payload.event.user, as_user: false, username: 'Hola Burrito' }).then(res => {
+			  //console.log( 'Successfully answered the command' );
+			  }).catch(console.error);
+			  return;
+		  }
+		  
+		  if (burritosRemainingPerDay(payload.event.user) <= 0) {
+			  slack.send({ token: BOT_TOKEN, text: 'You are out of burritos to give today.', channel: payload.event.user, as_user: false, username: 'Hola Burrito' }).then(res => {
+			  //console.log( 'Successfully answered the command' );
+			  }).catch(console.error);
+			  return;
+		  }
+		  
+		  burritoGiven(payload.event.user, userGivenBurrito, burritosGiven);
+		  
+		  slack.send({ token: BOT_TOKEN, text: 'Hola, you gave a burrito to @' + userGivenBurrito + '. You have ' + burritosRemainingPerDay(payload.event.user) + ' burritos left to give today.', channel: payload.event.user, as_user: false, username: 'Hola Burrito' }).then(res => {
+			  //console.log( 'Successfully answered the command' );
 		  }).catch(console.error);
-		  return;
+		  
+		  slack.send({ token: BOT_TOKEN, text: 'Hola, you recieved a burrito from <@' + payload.event.user + '>. Overall you have ' + burriotsRecieved(userGivenBurrito) + ' burritos.', channel: '@' + userGivenBurrito, as_user: false, username: 'Hola Burrito' }).then(res => {
+			 // console.log( 'Successfully answered the command' );
+		  }).catch(console.error);
 	  }
-	  
-	  burritoGiven(payload.event.user, userGivenBurrito[1].replace('@', ''));
-	  
-	  slack.send({ token: BOT_TOKEN, text: 'Hola, you gave a burrito to ' + userGivenBurrito[0] + '. You have ' + burritosRemainingPerDay(payload.event.user) + ' burritos left to give today.', channel: payload.event.user, as_user: false, username: 'Hola Burrito' }).then(res => {
-		  //console.log( 'Successfully answered the command' );
-	  }).catch(console.error);
-	  
-	  slack.send({ token: BOT_TOKEN, text: 'Hola, you recieved a burrito from <@' + payload.event.user + '>. Overall you have ' + burriotsRecieved(userGivenBurrito[1]) + ' burritos.', channel: userGivenBurrito[1], as_user: false, username: 'Hola Burrito' }).then(res => {
-		 // console.log( 'Successfully answered the command' );
-	  }).catch(console.error);
   }
 });
 
