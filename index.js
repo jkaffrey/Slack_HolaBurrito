adceabe2cfdafb3abe90faa79d20eb7e6618f222
Jeremy Kaffrey <jkaffrey@gmail.com>
2019-02-15 12:10:47 -0700

    Fixing subPath

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const ts = require('tinyspeck'),
 const burritoName = "burritos:";
 // setting defaults for all Slack API calls
 let slack = ts.instance({ token: BOT_TOKEN });
-let subPath = "burritos/";
+let subPath = "";
 
 function burritoGiven(fromUser, toUser, numberGiven) {
 
