a81948709d7bf169fb269636e68c77417f44851b
Jeremy Kaffrey <jkaffrey@gmail.com>
2020-03-23 12:07:26 -0600

    syntax

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const burritoName = "burritos:";
 // setting defaults for all Slack API calls
 let slack = ts.instance({ token: BOT_TOKEN });
 //let uri = 'mongodb://user:pass@host:port/dbname';
-let uri = 'mongodb://' + MONGODB_USER = ':' + MONGODB_PASS + '@' + MONGODB_URI;
+let uri = 'mongodb://' + MONGODB_USER + ':' + MONGODB_PASS + '@' + MONGODB_URI;
 
 mongodb.MongoClient.connect(uri, function(err, client) {
 
