6e8142ce5668b1daed5e70206da322885f49de79
Jeremy Kaffrey <jkaffrey@gmail.com>
2020-03-23 12:36:51 -0600

    This works for some reason

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const ts = require('tinyspeck'),
 const burritoName = "burritos:";
 // setting defaults for all Slack API calls
 let slack = ts.instance({ token: BOT_TOKEN });
-//let uri = encodeURI('mongodb://holaBurrito:eZRPtdQDZ2QZpX@ds139960.mlab.com:39960/heroku_8k5h3x81');
-let uri = encodeURI('mongodb://' + MONGODB_USER + ':' + MONGODB_PASS + '@ds139960.mlab.com:39960/heroku_8k5h3x81');
+let uri = encodeURI('mongodb://holaBurrito:eZRPtdQDZ2QZpX@ds139960.mlab.com:39960/heroku_8k5h3x81');
+//let uri = encodeURI('mongodb://' + MONGODB_USER + ':' + MONGODB_PASS + '@ds139960.mlab.com:39960/heroku_8k5h3x81');
 console.log(uri);
 
 mongodb.MongoClient.connect(uri, function(err, client) {
