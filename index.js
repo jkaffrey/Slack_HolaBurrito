4b14bb4561266177bcb3f9faea4c278926db1290
Jeremy Kaffrey <jkaffrey@gmail.com>
2020-03-23 12:57:29 -0600

    changes

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ mongodb.MongoClient.connect(uri, function(err, client) {
 
         for (var i = 0; i < numberGiven; i++) {
 
-            burritosReceived.update({ slackUser : recievedABurrito }, { $inc : { count : 1 }}, { upsert : true });
-            burritosGiven.update({ slackUser : gaveABurrito }, { $inc : { count : 1 }}, { upsert : true });
+            burritosReceived.findOneAndUpdate({ slackUser : recievedABurrito }, { $inc : { count : 1 }}, { upsert : true });
+            burritosGiven.findOneAndUpdate({ slackUser : gaveABurrito }, { $inc : { count : 1 }}, { upsert : true });
         }
     };
 
