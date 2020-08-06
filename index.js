478da9bfe722888a2e6c74e046285d9bc1985ec2
Jeremy Kaffrey <jkaffrey@gmail.com>
2020-08-05 22:59:23 -0600

    test

--- index.js
+++ index.js
@@ -465,7 +465,8 @@ mongodb.MongoClient.connect(uri, function(err, client) {
         var emoteType;
         if (payload.event.text && payload.event.text.indexOf(':burrito:') > 0 && payload.event.text.indexOf(':cannon:') > 0) {
             emoteType = 'burritoCannon';
-        } else if (payload.event.text && payload.event.text.indexOf(':burrito:') >= 0 && payload.event.text.indexOf(':bulbie:') >= 0) {
+        } else if (payload.event.text && payload.event.text.indexOf('@here') >= 0 && payload.event.text.indexOf(':burrito:') >= 0
+            && payload.event.text.indexOf(':bulbie:') >= 0) {
             emoteType = 'burritosForAll';
             console.log('burritos for all');
         } else if (payload.event.text && payload.event.text.indexOf(':burrito:') > 0) {
